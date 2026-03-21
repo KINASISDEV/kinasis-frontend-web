0bc239a0511e071fb6d6245901465dc1946bc793
Jacobo Monroy <162770749+KillahBlitz@users.noreply.github.com>
2026-03-21 16:51:28 -0600

    feat(calls.js): added new method call

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,13 +3,15 @@ import Menu from '../components/Menu/Menu.vue'
 import ContactUs from '../components/ContactUs/ContactUs.vue'
 import store from '../components/Store/Store.vue'
 import PrincipalPage from '../components/Kinasis/PrincipalPage.vue'
+import AboutUs from '../components/Kinasis/AboutUs.vue'
 
 
 const routes = [
   {path: '/', name: 'Home', component: Menu},
   {path: '/contact', name: 'Contact', component: ContactUs},
   {path: '/ecommerce', name: 'Ecommerce', component: store},
-  {path: '/website', name: 'website', component: PrincipalPage}
+  {path: '/website', name: 'website', component: PrincipalPage},
+  {path: '/website/aboutus', name: 'about', component: AboutUs}
   
 ]
 
